79c2661ab163dc043ec1d322d0f559fac5dc765a
Harun Wangereka <harunwangereka@gmail.com>
2021-09-06 23:58:24 +0300

    Added default channel and messages setup

--- app/src/main/java/io/getstream/streamcomposeattachements/MainActivity.kt
+++ app/src/main/java/io/getstream/streamcomposeattachements/MainActivity.kt
@@ -7,21 +7,60 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.logger.ChatLogLevel
+import io.getstream.chat.android.client.models.User
+import io.getstream.chat.android.compose.ui.channel.ChannelsScreen
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.chat.android.livedata.ChatDomain
 import io.getstream.streamcomposeattachements.ui.theme.StreamComposeAttachementsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val client = ChatClient.Builder("b67pax5b2wdq", applicationContext)
+            .logLevel(ChatLogLevel.ALL)
+            .build()
+        ChatDomain.Builder(client, applicationContext).build()
+
+        val user = User(
+            id = "tutorial-droid",
+            extraData = mutableMapOf(
+                "name" to "Tutorial Droid",
+                "image" to "https://bit.ly/2TIt8NR",
+            ),
+        )
+
+        client.connectUser(
+            user = user,
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZHJvaWQifQ.NhEr0hP9W9nwqV7ZkdShxvi02C5PR7SJE7Cs4y7kyqg"
+        ).enqueue()
+
         setContent {
-            StreamComposeAttachementsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            ChatTheme {
+                ChannelsList {
+                    finish()
                 }
             }
         }
     }
+}
+
+@Composable
+fun ChannelsList(action: () -> Unit) {
+    val context = LocalContext.current
+
+    ChannelsScreen(
+        title = stringResource(id = R.string.app_name),
+        onItemClick = { channel ->
+            context.startActivity(MessagesActivity.getIntent(context, channel.cid))
+        },
+        onBackPressed = { action.invoke() }
+    )
 }
 
 @Composable
